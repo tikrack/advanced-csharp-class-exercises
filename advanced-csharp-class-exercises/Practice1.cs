22f4580637d1999d6f256c8eb48f3eb9bf9b4547
Tikrack <tikrackcode@gmail.com>
2026-06-16 08:26:23 +0330

    feat(practice1): finish practice one

--- advanced-csharp-class-exercises/Practice1.cs
+++ advanced-csharp-class-exercises/Practice1.cs
@@ -16,5 +16,62 @@ namespace advanced_csharp_class_exercises
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Calculates the final visit fee after applying supplementary insurance
+        /// and an optional discount percentage.
+        /// </summary>
+        /// <param name="basicVisitFee">The original visit fee before any deductions.</param>
+        /// <param name="hasSupplementaryInsurance">
+        /// Indicates whether supplementary insurance is available.
+        /// If true, 30% of the fee will be deducted.
+        /// </param>
+        /// <param name="discountPercentage">
+        /// The additional discount percentage to apply after insurance deduction.
+        /// Default value is 0.
+        /// </param>
+        /// <returns>
+        /// The final visit fee after all applicable deductions.
+        /// </returns>
+
+        double CalculateVisitFee(
+            int basicVisitFee,
+            bool hasSupplementaryInsurance = false,
+            int discountPercentage = 0
+        ){
+            double fee = basicVisitFee;
+
+            if (hasSupplementaryInsurance)
+                fee = fee - (30 * fee / 100);
+
+            fee = fee - (discountPercentage * fee / 100);
+
+            return fee;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double r1 = CalculateVisitFee(
+                basicVisitFee: 50000,
+                hasSupplementaryInsurance: false,
+                discountPercentage: 10
+            );
+
+            double r2 = CalculateVisitFee(
+                basicVisitFee: 15000,
+                hasSupplementaryInsurance: true,
+                discountPercentage: 0
+            );
+
+            double r3 = CalculateVisitFee(
+                basicVisitFee: 35000,
+                hasSupplementaryInsurance: true,
+                discountPercentage: 30
+            );
+
+            MessageBox.Show(r1.ToString());
+            MessageBox.Show(r2.ToString());
+            MessageBox.Show(r3.ToString());
+        }
     }
 }
