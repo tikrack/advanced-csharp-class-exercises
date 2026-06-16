6c3b778e79a6c6cbe60b20c96de17cfa0ed8eb53
Tikrack <tikrackcode@gmail.com>
2026-06-16 09:09:01 +0330

    feat(practice2): adding practice2 and finish it

--- advanced-csharp-class-exercises/Practice2.Designer.cs
+++ advanced-csharp-class-exercises/Practice2.Designer.cs
@@ -28,11 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(69, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Practice2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(269, 98);
+            this.Controls.Add(this.button1);
+            this.Name = "Practice2";
             this.Text = "Practice2";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
--- advanced-csharp-class-exercises/Practice2.cs
+++ advanced-csharp-class-exercises/Practice2.cs
@@ -16,5 +16,35 @@ namespace advanced_csharp_class_exercises
         {
             InitializeComponent();
         }
+
+        double CalculateBMI(double weight, double height, out string status)
+        {
+            double bmi = weight / (height * height);
+
+            if (bmi < 18.5)
+                status = "کمبود وزن";
+            else if (bmi < 25)
+                status = "نرمال";
+            else if (bmi < 30)
+                status = "اضافه وزن";
+            else
+                status = "چاقی";
+
+            return bmi;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string state;
+
+            double r1 = CalculateBMI(
+                weight: 83,
+                height: 1.75,
+                status: out state
+            );
+
+            MessageBox.Show($"BMI: {r1}");
+            MessageBox.Show(state);
+        }
     }
 }
